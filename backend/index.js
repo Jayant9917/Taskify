7522e1fafbc60a10182c7648ce42236e3039c310
Jayant9917 <ranajayant527@gmail.com>
2025-05-15 15:35:56 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello bitch")
+});
+
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);;
+})
