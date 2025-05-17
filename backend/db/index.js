45a3bcdd9cf9094154bc5c75dade0eae7f247d54
Jayant9917 <ranajayant527@gmail.com>
2025-05-17 14:47:15 +0530

    adding the database schema

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+const objectId = mongoose.objectId;
+
+const userSchema = new Schema({
+    username : String,
+    password : String
+});
+
+const TodoSchema = new Schema({
+    userId : objectId,
+    title : String,
+    completed : Boolean
+});
+
+const user = mongoose.model('user', userSchema);
+const todo = mongoose.model('todo', TodoSchema);
+
+module.export = { 
+    user,
+    todo
+}
+
